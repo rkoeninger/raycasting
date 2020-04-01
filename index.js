ae64c733765d919668dc3f61909ebca73047a9af
Robert Koeninger <rkoeninger@att.net>
2020-04-01 01:56:12 -0400

    Added mouse tracing

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-const main = (g, w, h) => {
+const draw = (g, w, h, x, y) => {
+  g.fillStyle = 'darkgrey';
+  g.fillRect(0, 0, w, h);
+  g.fillStyle = 'black';
   g.fillRect(10, 10, w - 20, h - 20);
+  g.fillStyle = 'red';
+  g.fillRect(x - 5, y - 5, 10, 10);
 };
