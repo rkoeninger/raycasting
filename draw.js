e3420c60818b23f4d67c519bc76ce211890178e6
Robert Koeninger <rkoeninger@att.net>
2020-04-11 13:27:56 -0400

    Removed unused code

--- draw.js
+++ draw.js
@@ -15,119 +15,13 @@ const blue = '#268bd2';
 const cyan = '#2aa198';
 const green = '#859900';
 
-const lineIntersection = (x0, y0, x1, y1, x2, y2, x3, y3) => {
-  if (x0 === x1) {
-    if (x2 === x3) {
-      if (x0 === x2) {
-        return { x: x0, y: y1 }; // overlapping vertical
-      } else {
-        return undefined; // parallel vertical
-      }
-    } else {
-      const mb = (y3 - y2) / (x3 - x2);
-      const kb = y2 - mb * x2;
-      return { x: x0, y: mb * x0 + kb }; // only line a is vertical
-    }
-  } else if (x2 === x3) {
-    const ma = (y1 - y0) / (x1 - x0);
-    const ka = y0 - ma * x0;
-    return { x: x2, y: ma * x2 + ka }; // only line b is vertical
-  }
-
-  const ma = (y1 - y0) / (x1 - x0);
-  const mb = (y3 - y2) / (x3 - x2);
-  const ka = y0 - ma * x0;
-  const kb = y2 - mb * x2;
-
-  if (ma === mb) {
-    if (ka === kb) {
-      return { x: x0, y: y1 }; // overlapping
-    } else {
-      return undefined; // parallel
-    }
-  }
-
-  const x = (kb - ka) / (ma - mb);
-  const y = ma * x + ka;
-  return { x, y }; // intersection
-};
-
 const between = (k, a, b) => Math.min(a, b) <= k && k <= Math.max(a, b);
-
-const segmentIntersection = (x0, y0, x1, y1, x2, y2, x3, y3) => {
-  const p = lineIntersection(x0, y0, x1, y1, x2, y2, x3, y3);
-  if (p === undefined) {
-    return undefined;
-  } else if (between(p.x, x0, x1) && between(p.y, y0, y1) && between(p.x, x2, x3) && between(p.y, y2, y3)) {
-    return p;
-  }
-  return undefined;
-};
 
 const distanceSquared = (x0, y0, x1, y1) => {
   const dx = x1 - x0;
   const dy = y1 - y0;
   return dx * dx + dy * dy;
 };
-
-const drawRay = (g, x, y, mx, my) => {
-  let q, dsq, ql;
-  g.lineWidth = 1;
-  for (const { x0, y0, x1, y1 } of edges) {
-    if (x !== undefined && y !== undefined) {
-      const p = segmentIntersection(x0, y0, x1, y1, x, y, 10, 10);
-      if (p !== undefined) {
-        const dsp = distanceSquared(10, 10, p.x, p.y);
-        if (dsq === undefined || dsp < dsq) {
-          q = p;
-          dsq = dsp;
-          ql = { x0, y0, x1, y1 };
-        }
-      }
-    }
-    g.strokeStyle = base0;
-    g.beginPath();
-    g.moveTo(x0, y0);
-    g.lineTo(x1, y1);
-    g.closePath();
-    g.stroke();
-  }
-
-  if (q !== undefined) {
-    g.strokeStyle = red;
-    g.beginPath();
-    g.moveTo(ql.x0, ql.y0);
-    g.lineTo(ql.x1, ql.y1);
-    g.closePath();
-    g.stroke();
-  }
-
-  if (x !== undefined && y !== undefined) {
-    if (input.x !== undefined && input.y !== undefined) {
-      g.strokeStyle = base3;
-      g.lineWidth = 1;
-      g.beginPath();
-      g.moveTo(mx, my);
-      g.lineTo(input.x, input.y);
-      g.closePath();
-      g.stroke();
-    }
-
-    g.strokeStyle = violet;
-    g.lineWidth = 2;
-    g.beginPath();
-    if (q === undefined) {
-      g.moveTo(x, y);
-    } else {
-      g.moveTo(q.x, q.y);
-    }
-    g.lineTo(10, 10);
-    g.closePath();
-    g.stroke();
-  }
-};
-
-const range = (i, j) => [...new Array(j - i).keys()].map(x => x + i);
 
 const closestPointToCirlce = (cx, cy, px, py, qx, qy) => {
   const mpq = (qy - py) / (qx - px);
