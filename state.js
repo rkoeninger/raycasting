4f7ee157e863745b50735de4519e2c119fde27fb
Robert Koeninger <rkoeninger@att.net>
2020-06-09 11:47:23 -0400

    ESC to cancel input, initial shapes removed

--- state.js
+++ state.js
@@ -4,20 +4,9 @@ let selectedShape = 'box';
 
 let selectedAngle = 20;
 
-const circles = [
-  { x: 100, y: 100, r: 25 },
-  { x: 400, y: 150, r: 40 },
-  { x: 300, y: 200, r: 10 },
-  { x: 150, y: 350, r: 20 },
-  { x: 600, y: 400, r: 50 },
-  { x: 350, y: 600, r: 60 },
-  { x: 180, y: 500, r: 15 }
-];
+const circles = [];
 
-const boxes = [
-  { x: 300, y: 420, w: 50, h: 40 },
-  { x: 700, y: 200, w: 30, h: 75 }
-];
+const boxes = [];
 
 const inputShape = (offsetX, offsetY) => {
   if (input) {
