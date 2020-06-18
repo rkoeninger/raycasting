f6c8090ae1e074c43039b85c6d018a6fe8964d40
Robert Koeninger <rkoeninger@att.net>
2020-06-18 00:25:59 -0400

    Implemented LoS nearest box and cirlce intersections

--- draw.js
+++ draw.js
@@ -15,7 +15,96 @@ const blue = '#268bd2';
 const cyan = '#2aa198';
 const green = '#859900';
 
+const lineIntersection = (x0, y0, x1, y1, x2, y2, x3, y3) => {
+  if (x0 === x1) {
+    if (x2 === x3) {
+      if (x0 === x2) {
+        return { x: x0, y: y1 }; // overlapping vertical
+      } else {
+        return undefined; // parallel vertical
+      }
+    } else {
+      const mb = (y3 - y2) / (x3 - x2);
+      const kb = y2 - mb * x2;
+      return { x: x0, y: mb * x0 + kb }; // only line a is vertical
+    }
+  } else if (x2 === x3) {
+    const ma = (y1 - y0) / (x1 - x0);
+    const ka = y0 - ma * x0;
+    return { x: x2, y: ma * x2 + ka }; // only line b is vertical
+  }
+
+  const ma = (y1 - y0) / (x1 - x0);
+  const mb = (y3 - y2) / (x3 - x2);
+  const ka = y0 - ma * x0;
+  const kb = y2 - mb * x2;
+
+  if (ma === mb) {
+    if (ka === kb) {
+      return { x: x0, y: y1 }; // overlapping
+    } else {
+      return undefined; // parallel
+    }
+  }
+
+  const x = (kb - ka) / (ma - mb);
+  const y = ma * x + ka;
+  return { x, y }; // intersection
+};
+
+const between = (k, a, b) =>
+  (Math.min(a, b) <= k || Math.abs(k - Math.min(a, b)) <= 1) &&
+  (Math.max(a, b) >= k || Math.abs(k - Math.max(a, b)) <= 1);
+
+const segmentIntersection = (x0, y0, x1, y1, x2, y2, x3, y3) => {
+  const p = lineIntersection(x0, y0, x1, y1, x2, y2, x3, y3);
+  return (p !== undefined &&
+      between(p.x, x0, x1) &&
+      between(p.y, y0, y1) &&
+      between(p.x, x2, x3) &&
+      between(p.y, y2, y3))
+    ? p
+    : undefined;
+};
+
 const distance = (dx, dy) => Math.sqrt(dx * dx + dy * dy);
+
+const circleLineIntersection = (x0, y0, x1, y1, xc, yc, r) => {
+  x0 -= xc;
+  x1 -= xc;
+  y0 -= yc;
+  y1 -= yc;
+  const dx = x1 - x0;
+  const dy = y1 - y0;
+  const dr = distance(dx, dy);
+  const D = x0 * y1 - x1 * y0;
+  const sign = dy < 0 ? -1 : 1;
+  const dis = r * r * dr * dr - D * D;
+  if (dis < -0.1) {
+    return [];
+  } else if (Math.abs(dis) < 0.1) {
+    return [{
+      x: (D * dy) / (dr * dr) + xc,
+      y: (-1 * D * dx) / (dr * dr) + yc
+    }];
+  } else {
+    const xa = (D * dy + sign * dx * Math.sqrt(dis)) / (dr * dr) + xc;
+    const xb = (D * dy - sign * dx * Math.sqrt(dis)) / (dr * dr) + xc;
+    const ya = (-1 * D * dx + Math.abs(dy) * Math.sqrt(dis)) / (dr * dr) + yc;
+    const yb = (-1 * D * dx - Math.abs(dy) * Math.sqrt(dis)) / (dr * dr) + yc;
+    return [
+      { x: xa, y: ya },
+      { x: xa, y: yb },
+      { x: xb, y: ya },
+      { x: xb, y: yb }
+    ];
+  }
+};
+
+const circleSegmentIntersection = (x0, y0, x1, y1, xc, yc, r) =>
+  circleLineIntersection(x0, y0, x1, y1, xc, yc, r).filter(p =>
+    between(p.x, x0, x1) &&
+    between(p.y, y0, y1));
 
 const circleDistance = (c, p) => {
   const dx = p.x - c.x;
@@ -44,6 +133,27 @@ const translate = ({ x, y }, r, angle) => ({
   y: y + r * Math.sin(angle)
 });
 
+const closest = (p, qs) => {
+  let min, minDistance;
+  for (const q of qs) {
+    if (q) {
+      const d = distance(p.x - q.x, p.y - q.y);
+      if (minDistance === undefined || d < minDistance) {
+        minDistance = d;
+        min = q;
+      }
+    }
+  }
+  return min;
+};
+
+const intersections = (p, q, x, y, w, h) => ([
+  segmentIntersection(p.x, p.y, q.x, q.y, x, y, x + w, y),
+  segmentIntersection(p.x, p.y, q.x, q.y, x + w, y, x + w, y + h),
+  segmentIntersection(p.x, p.y, q.x, q.y, x + w, y + h, x, y + h),
+  segmentIntersection(p.x, p.y, q.x, q.y, x, y + h, x, y)
+]);
+
 const drawShadows = (g, sw, sh, m) => {
   if (m && !input && (circles.length > 0 || boxes.length > 0)) {
     const ps = [];
@@ -69,10 +179,18 @@ const drawShadows = (g, sw, sh, m) => {
 const drawLineOfSight = (g, sw, sh, m) => {
   g.fillStyle = orange;
   g.dot(target, 8).fill();
-
   if (m && !input) {
     g.strokeStyle = yellow;
     g.line(m.x, m.y, target.x, target.y).stroke();
+    const i = closest(target, [
+      ...intersections(target, m, 0, 0, sw, sh),
+      ...boxes.flatMap(b => intersections(target, m, b.x - b.w, b.y - b.h, b.w * 2, b.h * 2)),
+      ...circles.flatMap(c => circleSegmentIntersection(target.x, target.y, m.x, m.y, c.x, c.y, c.r))
+    ]);
+    if (i) {
+      g.fillStyle = magenta;
+      g.dot(i, 8).fill();
+    }
   }
 };
 
